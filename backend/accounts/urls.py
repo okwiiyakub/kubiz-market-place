35963e08369aab2ec76a617a71fb09004f4e6363
okwii yakub <okwiiyakub@gmail.com>
2026-05-01 22:29:58 +0300

    Updated the accounts urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, LogoutAPIView, CurrentUserAPIView
+from .views import (
+    RegisterAPIView,
+    LoginAPIView,
+    LogoutAPIView,
+    CurrentUserAPIView,
+    ChangePasswordAPIView,
+)
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='customer-register'),
     path('login/', LoginAPIView.as_view(), name='customer-login'),
     path('logout/', LogoutAPIView.as_view(), name='customer-logout'),
     path('me/', CurrentUserAPIView.as_view(), name='current-user'),
+    path('change-password/', ChangePasswordAPIView.as_view(), name='change-password'),
 ]
