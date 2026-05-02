8ce9b9433e7b3c6390ebf3376abe32596a654f7b
okwii yakub <okwiiyakub@gmail.com>
2026-05-02 21:40:06 +0300

    Added admin order detail links

--- frontend/src/pages/AdminOrders.jsx
+++ frontend/src/pages/AdminOrders.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import api from "../api/api";
 import getCsrfToken from "../utils/getCsrfToken";
 import AdminLayout from "../layouts/AdminLayout";
@@ -107,6 +108,14 @@ function AdminOrders() {
                         </li>
                       ))}
                     </ul>
+                  </td>
+                  <td className="py-4 pr-4">
+                    <Link
+                      to={`/admin-orders/${order.id}`}
+                      className="bg-blue-600 text-white px-4 py-2 rounded-lg font-medium hover:bg-blue-700 transition"
+                    >
+                      View
+                    </Link>
                   </td>
                 </tr>
               ))}
