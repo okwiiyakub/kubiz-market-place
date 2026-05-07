5be3df022a8de8a5715036b86f86ebb7b2359f89
okwii yakub <okwiiyakub@gmail.com>
2026-05-07 19:36:30 +0300

    edited the whatsapp place holder text

--- frontend/src/components/FloatingWhatsappButton.jsx
+++ frontend/src/components/FloatingWhatsappButton.jsx
@@ -16,7 +16,7 @@ function FloatingWhatsAppButton() {
       className="fixed bottom-6 right-6 bg-green-500 text-white px-5 py-3 rounded-full shadow-xl hover:bg-green-600 hover:-translate-y-1 transition z-50 font-bold flex items-center gap-2"
     >
       <span className="w-3 h-3 bg-white rounded-full animate-pulse"></span>
-      WhatsApp Support
+      WhatsApp
     </a>
   );
 }
