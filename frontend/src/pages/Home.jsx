0a3cabbd26dd75e3cbbde9048f7ff39120e99f20
okwii yakub <okwiiyakub@gmail.com>
2026-05-07 20:12:18 +0300

    improved product greed density

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -163,7 +163,7 @@ function Home() {
                   </h2>
                 </div>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5">
                   {featuredProducts.map((product) => (
                     <ProductCard key={product.id} product={product} />
                   ))}
